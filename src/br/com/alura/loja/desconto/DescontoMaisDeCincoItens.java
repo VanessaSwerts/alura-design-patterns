a2426a469cc0355ccafb2171295a75af9e858797
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2021-07-08 17:23:55 -0300

    Adicionando o design pattern Template Method

--- src/br/com/alura/loja/desconto/DescontoMaisDeCincoItens.java
+++ src/br/com/alura/loja/desconto/DescontoMaisDeCincoItens.java
@@ -10,11 +10,13 @@ public class DescontoMaisDeCincoItens extends Desconto {
 		super(proximo);
 	}
 
-	public BigDecimal calcular(Orcamento orcamento) {
-		if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0)
-			return orcamento.getValor().multiply(new BigDecimal("0.05"));
+	public BigDecimal efetuarCalculo(Orcamento orcamento) {
+		return orcamento.getValor().multiply(new BigDecimal("0.05"));
+	}
 
-		return proximo.calcular(orcamento);
+	@Override
+	public boolean deveAplicar(Orcamento orcamento) {
+		return orcamento.getValor().compareTo(new BigDecimal("500")) > 0;
 	}
 
 }
--- src/br/com/alura/loja/desconto/DescontoValorMaiorQueQuinhentos.java
+++ src/br/com/alura/loja/desconto/DescontoValorMaiorQueQuinhentos.java
@@ -10,11 +10,14 @@ public class DescontoValorMaiorQueQuinhentos extends Desconto {
 		super(proximo);
 	}
 
-	public BigDecimal calcular(Orcamento orcamento) {
-		if (orcamento.getQuantidadeItens() > 5)
-			return orcamento.getValor().multiply(new BigDecimal("0.1"));
+	public BigDecimal efetuarCalculo(Orcamento orcamento) {
+		
+		return orcamento.getValor().multiply(new BigDecimal("0.1"));
+	}
 
-		return proximo.calcular(orcamento);
+	@Override
+	public boolean deveAplicar(Orcamento orcamento) {
+		return orcamento.getQuantidadeItens() > 5;
 	}
 
 }
